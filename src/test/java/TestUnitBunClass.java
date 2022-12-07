68a891a1390cee031886b30445e733c2ffd38e81
Dmitry Timonin <dimkatort10@gmail.com>
2022-12-07 23:03:45 +0300

    Добавил параматризацию для гетера

--- src/test/java/TestUnitBunClass.java
+++ src/test/java/TestUnitBunClass.java
@@ -1,26 +1,49 @@
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import praktikum.Bun;
 
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
 public class TestUnitBunClass {
+    private Bun bun;
+    private final String nameExpected;
+    private final float priceExpected;
+
+    public TestUnitBunClass(String nameExpected, float priceExpected) {
+        this.nameExpected = nameExpected;
+        this.priceExpected = priceExpected;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getKittensCountData() {
+        return new Object[][]{
+                {"", Float.MAX_VALUE},
+                {"!@#!$%^&*()_~", 0f},
+                {"null", -99999f},
+                {"234234234", 345323.533f},
+                {"ОЧЕНЬ ДЛИННОЕ НАЗВАНИЕ КОТОРОЕ Я НЕ СМОГ ПРИДУМАТЬ НО ОЧЕНЬ СТАРАЛСЯ", 1f},
+                {"0", Float.MIN_VALUE},
+        };
+    }
 
     @Test
-    public void getNameBunTest(){
-    Bun bun = new Bun("Булочка",  44);
+    public void createBunGetNameTest() {
+        bun = new Bun(nameExpected, priceExpected);
 
-    String expected = "Булочка";
-    String actual = bun.getName();
-
-        Assert.assertEquals(expected,actual);
+        Assert.assertEquals(nameExpected, bun.getName());
 
     }
 
     @Test
-    public void getPriceBunTest(){
-        Bun bun = new Bun("Булочка",  44f);
-        float expected = 44f;
-        float actual = bun.getPrice();
+    public void createBunGetPriceTest() {
+        bun = new Bun(nameExpected, priceExpected);
 
-        Assert.assertEquals(expected,actual,0);
+        assertEquals(priceExpected, bun.getPrice(), 0);
     }
+
+
 }
